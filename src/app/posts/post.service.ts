da1278b835aad90c829a5ab6f5893c2886e90583
ahmad rami <ahmadrami@ahmads-MacBook-Pro.local>
2018-05-14 21:17:47 +0100

    updated to angular 6 with hmr

--- src/app/posts/post.service.ts
+++ src/app/posts/post.service.ts
@@ -23,24 +23,9 @@ export class PostService {
   getData(): Observable<any> {
     return this.http.get<Post>(`${this.endPoint}posts`)
       .pipe(
-        retry(3), // retry a failed request up to 3 times
-        catchError(this.handleError)
+        retry(3) // retry a failed request up to 3 times
       );
   }
 
-  private handleError(error: HttpErrorResponse) {
-    if (error.error instanceof ErrorEvent) {
-      // A client-side or network error occurred. Handle it accordingly.
-      console.error('An error occurred:', error.error.message);
-    } else {
-      // The backend returned an unsuccessful response code.
-      // The response body may contain clues as to what went wrong,
-      console.error(
-        `Backend returned code ${error.status}, ` +
-        `body was: ${error.error}`);
-    }
-    // return an ErrorObservable with a user-facing error message
-    return new ErrorObservable(
-      'Something bad happened; please try again later.');
-  }
+  
 }
